4f04949697806eca75424e94d4eac9fe04a3a069
nannany <ymym1990ymym@gmail.com>
2021-10-27 14:20:17 +0900

    advice変更

--- src/main/java/com/example/demo/advice/MyControllerAdvice.java
+++ src/main/java/com/example/demo/advice/MyControllerAdvice.java
@@ -1,14 +1,26 @@
 package com.example.demo.advice;
 
+import com.example.demo.record.MyResponse;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.AbstractMappingJacksonResponseBodyAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdviceAdapter;
+
+import java.io.IOException;
 
 @RestControllerAdvice
-public class MyControllerAdvice {
+public class MyControllerAdvice extends AbstractMappingJacksonResponseBodyAdvice {
 
-    @ModelAttribute
-    public void systemOut() {
-        System.out.println("rest controller advice");
+
+    @Override
+    protected void beforeBodyWriteInternal(MappingJacksonValue bodyContainer, MediaType contentType, MethodParameter returnType, ServerHttpRequest request, ServerHttpResponse response) {
+
+        System.out.println("my controller advice");
+
     }
-
 }
